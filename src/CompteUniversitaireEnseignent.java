963924e8c139a1846cbac4599475886e873dba3a
kaoutar_sarsari <kaoutarsarsari@gmail.com>
2024-10-09 19:48:24 +0100

    Creation des class du tp 0

---
+++ src/CompteUniversitaireEnseignent.java
@@ -0,0 +1,13 @@
+public class CompteUniversitaireEnseignent {
+    private Enseignent enseignent;
+    private String login;
+
+    public CompteUniversitaireEnseignent(Enseignent enseignent) {
+        super();
+        this.enseignent = enseignent;
+    }
+    public void genererLogin(){
+        String login = this.enseignent.getPrenom()+this.enseignent.getNom() + this.enseignent.getGrade()+"usms.ma";
+        this.login = login;
+    }
+}
---
+++ src/CompteUniversitaireEtudiant.java
@@ -0,0 +1,13 @@
+public class CompteUniversitaireEtudiant {
+    private Etudiant etudiant;
+    private String login;
+
+    public CompteUniversitaireEtudiant(Etudiant etudiant) {
+        super();
+        this.etudiant = etudiant;
+    }
+    public void genererLogin(){
+        String login = this.etudiant.getPrenom().charAt(0)+this.etudiant.getNom() + "usms.ac.ma";
+        this.login = login;
+    }
+}
